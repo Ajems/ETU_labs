8a9e81f6132031dd989e66d9734d20218ab8a415
Ajems (Danil Korenev) <70469206+Ajems@users.noreply.github.com>
2022-12-02 14:14:46 +0400

    Delete object_oriented_programming/Runtime directory

--- object_oriented_programming/Runtime/Log/output/ConsoleLog.cpp
+++
@@ -1,6 +0,0 @@
-#include <iostream>
-#include "ConsoleLog.h"
-
-void ConsoleLog::output(Message* msg) {
-    std::cout << *msg << '\n';
-}
--- object_oriented_programming/Runtime/main.cpp
+++
@@ -1,7 +0,0 @@
-#include "Interaction/Mediator.h"
-
-int main() {
-    Mediator mediator = Mediator();
-    mediator.start();
-    return 0;
-};
