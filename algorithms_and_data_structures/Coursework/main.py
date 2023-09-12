b2306acd9e61937996f1e581bc17c6d3c029fc7f
Ajems (Danil Korenev) <70469206+Ajems@users.noreply.github.com>
2023-09-12 17:57:38 +0300

    Update main.py

--- algorithms_and_data_structures/Coursework/main.py
+++ algorithms_and_data_structures/Coursework/main.py
@@ -12,20 +12,7 @@ def main():
         print("remove {}".format(i))
         my_table.remove(str(i))
 
-
     print("ff")
-
-    # rb_tree = RBTree()
-    # rb_tree.insert("down", "Yes, I'm!")
-    # rb_tree.insert("down.", "No, I'm gay!")
-    # # rb_tree.print_tree()
-    # print(rb_tree.get("down"))
-    # print()
-    # rb_tree.remove("down")
-    # # rb_tree.print_tree()
-
-
-
-
+    
 if __name__ == '__main__':
     main()
